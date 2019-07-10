b29b801fbdb4c2e72e8d72dc4c9aa3d223637617
Jarren Bird <adiofan@gmail.com>
2019-07-10 11:55:57 -0600

    added airtable info to gatsby-config.js

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -27,6 +27,20 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // below is airtable info for gatsby-source-airtable
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: `keyO8SssyduM50AlX`, // may instead specify via env, see below
+        tables: [
+          {
+            baseId: `apprTfFEvOaNEFI5i`,
+            tableName: `keepers`,
+            tableView: `Keepers`, // optional
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
