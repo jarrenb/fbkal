da7e5f64f90b8189fcd09517d56a2ac61e2a5989
Jarren Bird <adiofan@gmail.com>
2019-07-16 16:01:58 -0600

    working on showing all keepers on individual tables without using a component. just having it all generated from one graphql query in the keepers page

--- gatsby/src/pages/keepers.js
+++ gatsby/src/pages/keepers.js
@@ -4,26 +4,39 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import KeepersTeamTable from "../components/keepers-team-table"
 
-export default ({ data }) => {
+const Keepers = ({ data }) => {
+  // grab all the teams from each keeper
+  const allKeepersTeamsArray = data.allAirtable.edges.map(
+    edge => edge.node.data.team
+  )
+  // use Set to remove duplicates from allKeepersTeamsArray
+  const eachTeamSet = new Set(allKeepersTeamsArray)
+  // turn Set back into an array
+  const eachTeamArray = Array.from(eachTeamSet)
   return (
     <Layout>
       <div>
         <h2>Keepers</h2>
-        {data.allAirtable.edges.map(({ node }, index) => (
-          <KeepersTeamTable key={index} teamName={node.data.name} />
+        {eachTeamArray.map(team => (
+          <section>
+            <h3>{team}</h3>
+          </section>
         ))}
       </div>
     </Layout>
   )
 }
 
+export default Keepers
+
 export const query = graphql`
   {
-    allAirtable(filter: { table: { eq: "teams" } }) {
+    allAirtable(filter: { table: { eq: "keepers" } }) {
       edges {
         node {
           data {
-            name
+            Player_Name__Team___Position_
+            team
           }
         }
       }
--- readme.md
+++ readme.md
@@ -4,6 +4,6 @@ a site for the fantasy basketball keepers auction league. specifically the one i
 
 # todo
 
-- move airtable API key to process.env so that it's hidden
 - move keepers-team-table back into keepers page (i didn't need to make a component. i thought i did, but i was wrong)
+- move airtable API key to process.env so that it's hidden
 - style table to match steph's design
