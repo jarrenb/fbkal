0be6c0d27130afae324598ca9ca58624518fd834
Jarren Bird <adiofan@gmail.com>
2019-10-09 11:40:09 -0600

    added a breadcrumb prop to the 404 page

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -3,12 +3,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const breadcrumbs = [{ to: "/", title: "FBKAL" }]
+
+  return (
+    <Layout breadcrumbs={breadcrumbs}>
+      <SEO title="404: Not found" />
+      <h2>NOT FOUND</h2>
+      <p>You just requested a page that doesn&#39;t exist. I'm sorry.</p>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
